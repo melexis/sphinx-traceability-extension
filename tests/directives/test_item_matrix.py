7d999854453f99c659e3fb6565afa7e6f30890fd
Jasper Craeghs <jasper.craeghs@gmail.com>
2021-07-18 17:10:17 +0200

    Update unit test

--- tests/directives/test_item_matrix.py
+++ tests/directives/test_item_matrix.py
@@ -2,29 +2,35 @@ from collections import namedtuple
 from unittest import TestCase
 
 from docutils import nodes
-from mlx.directives.item_matrix_directive import ItemMatrix as dut
+from mlx.directives.item_matrix_directive import ItemMatrix
 
 from parameterized import parameterized
 
 
 class TestItemMatrix(TestCase):
-    Rows = namedtuple('Rows', "sorted covered uncovered")
+    Rows = namedtuple('Rows', "sorted covered uncovered counters")
 
     @parameterized.expand([
-       (True, False, [1, 1, 0]),
-       (False, False, [1, 0, 1]),
-       (True, True, [1, 1, 0]),
-       (False, True, [0, 0, 1]),
+        (True, False, True, [0, 0], [1, 1, 0]),
+        (False, False, True, [0, 0], [1, 0, 1]),
+        (True, True, True, [0, 0], [1, 1, 0]),
+        (False, True, True, [0, 0], [0, 0, 1]),
     ])
-    def test_store_row(self, covered, onlycovered, expected_lengths):
-        rows = self.Rows([], [], [])
+    def test_store_data(self, covered, onlycovered, splittargets, attributes, expected_lengths):
+        dut = ItemMatrix()
+        dut['onlycovered'] = onlycovered
+        dut['intermediate'] = ''
+        dut['sourceattributes'] = ['attr'] * attributes[0]
+        dut['targetattributes'] = ['attr'] * attributes[1]
+        dut['splittargets'] = splittargets
+        rows = self.Rows([], [], [], [0, 0])
         left = nodes.entry('left')
         rights = [nodes.entry('right1'), nodes.entry('right2')]
-        dut._store_row(rows, left, rights, covered, onlycovered)
+        dut._store_data(rows, left, rights, covered, None)
 
-        self.assertEqual([len(attr) for attr in rows], expected_lengths)
+        self.assertEqual([len(attr) for attr in rows[:3]], expected_lengths)
         my_row = nodes.row()
         my_row += left
         my_row += rights
-        for idx, rows_per_type in enumerate(rows):  # verify that rows contain the three entries
+        for idx, rows_per_type in enumerate(rows[:3]):  # verify that rows contain the three entries
             self.assertEqual(str(rows_per_type), str([my_row] * expected_lengths[idx]))
