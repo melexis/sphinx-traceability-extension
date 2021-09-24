9eb3e7bb9ff17461ddb30411ef50c0d43a8bcd9d
jce <28319872+JasperCraeghs@users.noreply.github.com>
2021-09-24 16:46:18 +0200

    Tweak error message for consistency

--- mlx/directives/item_tree_directive.py
+++ mlx/directives/item_tree_directive.py
@@ -59,7 +59,7 @@ class ItemTree(TraceableBaseNode):
                     try:
                         childcontent.append(self._generate_bullet_list_tree(app, collection, target))
                     except RecursionError as err:
-                        msg = ("Could not process item-tree {!r} because of a circular relationship: {} -{}-> {}"
+                        msg = ("Could not process item-tree {!r} because of a circular relationship: {} {} {}"
                                .format(self['title'], item_id, relation, target))
                         raise TraceabilityException(msg) from err
         bullet_list_item.append(childcontent)
