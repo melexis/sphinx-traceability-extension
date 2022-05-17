8c7c74c30e607424188eb3ad7c5534b4724d65cf
jce <28319872+JasperCraeghs@users.noreply.github.com>
2022-05-17 11:56:39 +0200

    Call callback function 'traceability_inspect_item' after rendering the item as it shouldn't be used to modify the item

--- mlx/directives/item_directive.py
+++ mlx/directives/item_directive.py
@@ -20,8 +20,6 @@ class Item(TraceableBaseNode):
         """
         self._item = collection.get_item(self['id'])
         item_id = self._item.get_id()
-        if app.config.traceability_inspect_item:
-            app.config.traceability_inspect_item(item_id, collection)
         top_node = self.create_top_node(item_id, app=app)
         dl_node = nodes.definition_list()
         if app.config.traceability_render_attributes_per_item:
@@ -32,6 +30,8 @@ class Item(TraceableBaseNode):
             top_node.append(dl_node)
         # Note: content should be displayed during read of RST file, as it contains other RST objects
         self.replace_self(top_node)
+        if app.config.traceability_inspect_item:
+            app.config.traceability_inspect_item(item_id, collection)
 
     def _process_attributes(self, dl_node, app):
         """ Processes all attributes for the given item and adds the list of attributes to the given definition list.
