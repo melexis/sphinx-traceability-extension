01e4e44e4754b05071ef1de393b6874b7707dd43
jce <28319872+JasperCraeghs@users.noreply.github.com>
2022-06-23 12:28:03 +0200

    Report warning when item-piechart does not contain anything and prevent matplotlib from producing a blank image and a DeprecationWarning

--- mlx/directives/item_pie_chart_directive.py
+++ mlx/directives/item_pie_chart_directive.py
@@ -72,7 +72,11 @@ class ItemPieChart(TraceableBaseNode):
                            self['document'], self['line'])
         p_node = nodes.paragraph()
         p_node += nodes.Text(statistics)
-        top_node += self.build_pie_chart(chart_labels, env)
+        if chart_labels:
+            top_node += self.build_pie_chart(chart_labels, env)
+        else:
+            report_warning("item-piechart does not contain any items: no image will be generated",
+                           self['document'], self['line'])
         top_node += p_node
         self.replace_self(top_node)
 
