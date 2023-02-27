4c3f095f1bb636f9f77629ed83445ab30996d206
jce <jce@melexis.com>
2023-02-27 12:03:38 +0100

    Move new check and remove old check

--- mlx/directives/item_pie_chart_directive.py
+++ mlx/directives/item_pie_chart_directive.py
@@ -58,11 +58,6 @@ class ItemPieChart(TraceableBaseNode):
         self.target_relationships = self['targettype'] if self['targettype'] else self.collection.iter_relations()
         self.relationship_to_string = app.config.traceability_relationship_to_string
         self._set_priorities()
-        if self['colors'] and len(self['colors']) < len(self.priorities):
-            report_warning("item-piechart can contain up to {} slices but only {} colors have been provided: some "
-                           "colors may be reused".format(len(self.priorities), len(self['colors'])),
-                           self['document'], self['line'])
-
         self._set_nested_target_regex()
         target_regex = re.compile(self['id_set'][1])
         for source_id in self.collection.get_items(self['id_set'][0], self['filter-attributes']):
@@ -74,15 +69,13 @@ class ItemPieChart(TraceableBaseNode):
             self.loop_relationships(source_id, source_item, self.source_relationships, target_regex,
                                     self._match_covered)
 
+        if self['colors'] and len(self['colors']) < len(self.priorities):
+            report_warning("item-piechart can contain up to {} slices but only {} colors have been provided: some "
+                           "colors may be reused".format(len(self.priorities), len(self['colors'])),
+                           self['document'], self['line'])
         data, statistics = self._prepare_labels_and_values(self.priorities,
                                                            list(self.linked_labels.values()),
                                                            self['colors'])
-
-        if data['colors'] and len(data['colors']) < len(data['labels']):
-            report_warning("item-piechart contains {} slices: {} color(s) will be reused"
-                           .format(len(data['labels']), len(data['labels']) - len(data['colors'])),
-                           self['document'], self['line'])
-
         if data['labels']:
             top_node += self.build_pie_chart(data['sizes'], data['labels'], data['colors'], env)
 
