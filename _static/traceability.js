f19084f1a7ac7f100f1732b9e33723f9805278fc
Letme <crt.mori@gmail.com>
2024-07-31 20:44:05 +0000

    Deploying to gh-pages from @ melexis/sphinx-traceability-extension@3fd420bdb4fc5ccbafcff9eb895e6efbc995faeb 🚀

--- _static/traceability.js
+++ _static/traceability.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
             'white-space': 'pre'  // prevents adding newlines
         });
         $(this).hover(
-            function() {
+            function () {
                 // entering hover state
                 caption.show();
                 var captionRight = caption.offset().left + caption.outerWidth();
@@ -48,10 +48,36 @@ $(document).ready(function () {
                     // lines up the caption behind the item ID
                     caption.css('transform', 'translate(0.3rem, -3px)');
                 }
-            }, function() {
+            }, function () {
                 // leaving hover state
                 caption.css('transform', 'none');  // resets the transformations
                 caption.hide();
+            }
+        );
+    });
+
+    $('table > tbody > tr[class^=item-group-]').each(function (i) {
+        $(this).on("contextmenu",
+            function (event) {
+                event.preventDefault()
+                var groupName = /item-group-\d+/.exec($(this).attr('class'))[0];
+                $(this).parent().find(`tr.${groupName} > td > p.item-link`).each(function (j) {
+                    const cell = $(this).parent();
+                    const maxWidth = Math.max(cell.width(), ($("div.rst-content").width() / 2));
+                    cell.css("maxWidth", maxWidth);
+                    const content = $(this).children('div.content').first();
+                    if (content.length) {
+                        content.toggle();
+                    } else {
+                        var link = $(this).children('a').first();
+                        var container = $('<div>', { class: 'content' });
+                        var paragraph = $(this)
+                        container.load(link.attr('href').replace('#', ' #content-'), function () {
+                            container.find('*').css("width", "inherit");
+                            paragraph.append(container);
+                        });
+                    }
+                });
             }
         );
     });
