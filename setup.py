06fa61c6c1f31b393435a3caa89e1e787fe357f6
JWM <jwm@melexis.com>
2024-09-10 17:28:10 +0200

    Drop support for Python 3.7

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
